b379eb3adfb6e0b243fbd0080c9aba281a2e6d21
kaylahr-28 <kaylahrouzier28@gmail.com>
2025-10-30 08:53:57 -0400

    added comments explaining process

--- practicing-software-dev/practicing-software-dev/Program.cs
+++ practicing-software-dev/practicing-software-dev/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace practicing_software_dev
 {
-    internal class Program
+    internal class Restaurant
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //initialize Queue "Orders"
+            //welcome text
+            //while-loop: while user input != empty line, "what can i get you" , prompt for dish name, size/quantity, who it's for etc.
+                //for each line of user input, make sure its correct numbers/type/etc
+                //make Order object for each user input of an order, add to Queue
+            //"kitchen making orders yay"
+            //for loop: dequeues to "make" orders
+            //print " heres your order (dish, quantity, name) yippiee"
+            //"enjoy!"
         }
     }
+
+    //2nd internal class Order - make one object for every order they make, store in Queue
 }
